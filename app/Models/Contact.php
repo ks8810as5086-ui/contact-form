3a052768dc6ecac0b9b066136c2585acbd63df3b
ks8810as5086-ui <ks.8810.as.5086@gmail.com>
2026-06-04 10:50:08 +0900

    feat: 各モデルにリレーション定義とfillable設定を追加 closes #2

---
+++ app/Models/Contact.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['category_id',
+        'first_name',
+        'last_name',
+        'gender',
+        'email',
+        'tel',
+        'address',
+        'building',
+        'detail',
+    ];
+
+    // お問い合わせは1つのカテゴリに属する
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // お問い合わせは複数のタグを持つ
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
